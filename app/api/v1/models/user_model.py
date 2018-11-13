502e5531f0eec194c15acfe93b1fe93516f961cb
RachelleMaina <rachellemaina@gmail.com>
2018-11-13 23:42:49 +0300

    [Finishes #161935248] Added cancel Order functionality

--- app/api/v1/models/user_model.py
+++ app/api/v1/models/user_model.py
@@ -20,4 +20,23 @@ class UserModel(object):
         }
         self.users.append(payload)
 
- 
+    def create_order(self, pickup_location, destination, weight, quote, status):
+        """"Method to create a parcel order deliveries."""
+
+        payload = {
+            "order_id": len(self.orders) + 1,
+            "user_id": len(self.users) + 1,
+            "pickup_location": pickup_location,
+            "destination": destination,
+            "weight": weight,
+            "quote": quote,
+            "status": status
+        }
+        self.orders.append(payload)
+
+    def cancel_order(self, parcelId):
+        """"Method to cancel a parcel order delivery."""
+        new_order = next((order for order in self.orders if order[
+            "order_id"] == parcelId), None)
+        new_order["status"] = "cancelled"
+        return new_order
