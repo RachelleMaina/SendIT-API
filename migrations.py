a150bee92d5e3705a2003c418862f2b57056a06f
RachelleMaina <43179925+RachelleMaina@users.noreply.github.com>
2018-11-30 12:30:17 +0300

    Update migrations.py

--- migrations.py
+++ migrations.py
@@ -1,7 +1,6 @@
 from instance.db_config import create_tables, destroy_tables, create_admin, create_user, create_order, db_init
 
 def setup_db():
-    # db_init()
     destroy_tables()
     create_tables()
     create_admin()
